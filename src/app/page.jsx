320049bd15d35a1acfd69ec545dbd69e6b047a50
Aviral <aviral.kochgaway@gmail.com>
2023-11-06 14:03:05 +0530

    dd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,13 @@ import Navbar from './components/Navbar'
 export default function Home() {
   return (
     <body>
-      <div className='topbar'>
-      <Navbar />
+      <div className='center'>
+        <div className='topbar'>
+          <Navbar />
+        </div>
+      </div>
+      <div className='top1'>
+        <h1>Why us?</h1>
       </div>
     </body>
   )
