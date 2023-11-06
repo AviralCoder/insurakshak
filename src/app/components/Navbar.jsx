e63e595ce66e9f2d4307f8c9cdf9752c0078eab6
Aviral <aviral.kochgaway@gmail.com>
2023-11-06 14:02:58 +0530

    background

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return (
+        <div className="nav">
+            <button>About Us</button>
+            <button>Shop</button>
+            <button>Contact</button>
+        </div>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+import Navbar from './components/Navbar'
+
+export default function Home() {
+  return (
+    <body>
+      <div className='topbar'>
+      <Navbar />
+      </div>
+    </body>
+  )
+}
